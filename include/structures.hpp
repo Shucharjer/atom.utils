667598cf8b66c38c9a20740eac08f79fe246bb44
Shucharjer <2592284636@qq.com>
2025-01-22 12:56:00 +0800

    added some allocator templates

--- include/structures.hpp
+++ include/structures.hpp
@@ -3,6 +3,7 @@
 #include "concepts/allocator.hpp"
 #include "memory.hpp"
 #include "memory/allocator.hpp"
+#include "memory/pool.hpp"
 
 namespace atom::utils {
 
@@ -21,5 +22,23 @@ template <
         UTILS standard_allocator<UTILS compressed_pair<Key, Val>>,
     std::size_t = k_default_page_size>
 class dense_map;
+
+template <typename Ty>
+using sync_allocator = allocator<Ty, synchronized_pool>;
+
+template <typename First, typename Second>
+using sync_comperessed_allocator = sync_allocator<compressed_pair<First, Second>>;
+
+template <typename First, typename Second>
+using sync_pair_allocator = sync_allocator<std::pair<const First, Second>>;
+
+template <typename Ty>
+using unsync_allocator = allocator<Ty, unsynchronized_pool>;
+
+template <typename First, typename Second>
+using unsync_compressed_allocator = unsync_allocator<compressed_pair<First, Second>>;
+
+template <typename First, typename Second>
+using unsync_pair_allocator = sync_allocator<std::pair<const First, Second>>;
 
 } // namespace atom::utils
