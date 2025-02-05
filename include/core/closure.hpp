1ef8175c7a6aba9ea1e766e67aea6def1ea66e8d
Shucharjer <2592284636@qq.com>
2025-02-05 19:35:21 +0800

    adjustment for pipeline; added comment

--- include/core/closure.hpp
+++ include/core/closure.hpp
@@ -2,12 +2,19 @@
 #include <concepts>
 #include <tuple>
 #include "core.hpp"
-#include "core/pipline_result.hpp"
+#include "core/pipeline.hpp"
 
 namespace atom::utils {
 
+/**
+ * @brief Closure.
+ *
+ * It could be used as range adaptor closure.
+ * @tparam Fn Function called in operator()
+ * @tparam Args Arguments that
+ */
 template <typename Fn, typename... Args>
-class closure {
+class closure : public pipeline_base<closure<Fn, Args...>> {
     using index_sequence = std::index_sequence_for<Args...>;
     using self_type      = closure;
 
@@ -49,14 +56,9 @@ public:
         return call(*this, std::forward<Ty>(arg), index_sequence{});
     }
 
-    template <typename Closure>
-    constexpr auto operator|(Closure closure) && {
-        return pipeline_result<self_type, Closure>(std::move(*this), std::move(closure));
-    }
-
 private:
     template <typename SelfTy, typename Ty, size_t... Index>
-    static constexpr decltype(auto)
+    constexpr static decltype(auto)
         call(SelfTy&& self, Ty&& arg, std::index_sequence<Index...>) noexcept(noexcept(
             Fn{}(std::forward<Ty>(arg), std::get<Index>(std::forward<SelfTy>(self).args_)...)
         )) {
@@ -67,6 +69,14 @@ private:
     std::tuple<Args...> args_;
 };
 
+/**
+ * @brief Making a closure without caring about the parameter types.
+ *
+ * @tparam Fn Closure function type.
+ * @tparam Args Arguments types would be called in the closure. They could be deduced.
+ * @param args Arguments.
+ * @return closure<Fn, std::decay_t<Args>...> Closure.
+ */
 template <typename Fn, typename... Args>
 constexpr static auto make_closure(Args&&... args) -> closure<Fn, std::decay_t<Args>...> {
     return closure<Fn, std::decay_t<Args>...>(std::forward<Args>(args)...);
