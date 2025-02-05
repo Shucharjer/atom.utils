ced76e821e4139201cdb9eefa007db1f663239f8
Shucharjer <2592284636@qq.com>
2025-02-05 16:48:20 +0800

    removed forward declaration in ranges.hpp

--- include/ranges.hpp
+++ include/ranges.hpp
@@ -8,7 +8,4 @@ namespace atom::utils::ranges {
 template <std::ranges::input_range Rng>
 class phony_input_iterator;
 
-template <typename Rng, typename... Args>
-class closure;
-
 } // namespace atom::utils::ranges
