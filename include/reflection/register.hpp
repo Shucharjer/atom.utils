fa95ae956a3dcc9e6bd8037aa707c8180de4d28b
Shucharjer <2592284636@qq.com>
2025-02-05 13:40:49 +0800

    comments, more details

--- include/reflection/register.hpp
+++ include/reflection/register.hpp
@@ -3,10 +3,22 @@
 
 namespace atom::utils {
 
+/**
+ * @brief Register.
+ *
+ * Call the registry's registration function when creating an instance.
+ * @tparam Ty The type want to register.
+ * @tparam BasicConstexprExtend basic compile time extend information.
+ * @tparam ConstexprExtend Advanced compile time extend information.
+ */
 template <typename Ty, typename BasicConstexprExtend, template <typename> typename ConstexprExtend>
 struct type_register {
     type_register() { registry<BasicConstexprExtend, ConstexprExtend>::template enroll<Ty>(); }
-    ~type_register() noexcept = default;
+    type_register(const type_register&)            = delete;
+    type_register(type_register&&)                 = delete;
+    type_register& operator=(const type_register&) = delete;
+    type_register& operator=(type_register&&)      = delete;
+    ~type_register() noexcept                      = default;
 };
 
 } // namespace atom::utils
