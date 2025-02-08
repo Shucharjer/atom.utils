97aa3a7b8d096c59085136c96eb7bea66c5c3a7c
Shucharjer <2592284636@qq.com>
2025-02-08 10:34:22 +0800

    Update: changed `shared_type` in `synchronized_pool` to a raw pointer | 更新：将 `synchronized_pool`中的 `shared_type` 换成了原始指针

--- include/memory/pool.hpp
+++ include/memory/pool.hpp
@@ -23,82 +23,38 @@ constexpr bool is_pow_of_two(const SizeTy size) {
 } // namespace internal
 
 class synchronized_pool {
-public:
     using self_type = synchronized_pool;
-    class pool;
-    using shared_type = std::shared_ptr<pool>;
+public:
+    using shared_type = synchronized_pool*;
 
     using size_type = std::size_t;
 
-    class pool {
-        using memory_block = ::atom::utils::internal::memory_block;
-
-        friend class synchronized_pool;
-
-        pool() = default;
-
-        static auto get() -> std::shared_ptr<pool> { return std::shared_ptr<pool>(); }
-
-    public:
-        using size_type = synchronized_pool::size_type;
-
-        pool(const pool&)            = delete;
-        pool& operator=(const pool&) = delete;
-
-        pool(pool&& that)
-            : unused_blocks_(std::move(that.unused_blocks_)),
-              using_blocks_(std::move(that.using_blocks_)) {}
-
-        pool& operator=(pool&& that) {
-            if (this != &that) {
-                unused_blocks_ = std::move(that.unused_blocks_);
-                using_blocks_  = std::move(that.using_blocks_);
-            }
-            return *this;
-        }
-
-        ~pool() {}
-
-        template <typename Ty>
-        auto allocate(
-            const size_type count = 1, const std::align_val_t align = std::align_val_t{ 16 }
-        ) -> Ty* {
-            // TODO: allocate impl
-            return static_cast<Ty*>(operator new(sizeof(Ty) * count, align));
-        }
-
-        template <typename Ty>
-        void deallocate(
-            Ty* ptr,
-            const size_type count        = 1,
-            const std::align_val_t align = std::align_val_t{ 16 }
-        ) {
-            // TODO: deallocate impl
-            operator delete(static_cast<void*>(ptr), sizeof(Ty) * count, align);
-        }
-
-    private:
-        std::map<size_type, std::stack<memory_block>> unused_blocks_;
-        std::map<size_type, std::map<default_id_t, memory_block>> using_blocks_;
-    };
-
-    synchronized_pool() : pool_(pool::get()) {}
+    synchronized_pool() {}
     synchronized_pool(const synchronized_pool&)            = delete;
     synchronized_pool& operator=(const synchronized_pool&) = delete;
-    synchronized_pool(synchronized_pool&& that) : pool_(std::move(that.pool_)) {}
-    synchronized_pool& operator=(synchronized_pool&& that) {
-        if (this != &that) {
-            pool_ = std::move(that.pool_);
-        }
-        return *this;
-    }
+    synchronized_pool(synchronized_pool&& that)            = delete;
+    synchronized_pool& operator=(synchronized_pool&& that) = delete;
     ~synchronized_pool() = default;
 
-    auto get() -> shared_type { return pool_; }
-    auto get() const -> const shared_type { return pool_; }
+    auto get() -> shared_type { return this; }
+
+    template <typename Ty>
+    auto allocate(
+        const size_type count = 1, const std::align_val_t align = std::align_val_t{ 16 }
+    ) -> Ty* {
+        return static_cast<Ty*>(operator new(sizeof(Ty) * count, align));
+    }
+
+    template <typename Ty>
+    void deallocate(
+        Ty* const ptr,
+        const size_type count = 1,
+        const std::align_val_t align = std::align_val_t{ 16 }
+    ) noexcept {
+        operator delete(static_cast<void*>(ptr), sizeof(Ty) * count, align);
+    }
 
 private:
-    shared_type pool_;
 };
 
 class unsynchronized_pool {
