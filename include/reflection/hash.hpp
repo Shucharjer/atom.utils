dc8f83443256aeb3f493680223d0b21dea991c69
Shucharjer <2592284636@qq.com>
2025-02-13 17:26:37 +0800

    Remove: process before hash a type's name | 移除：对类型名哈希前的处理

--- include/reflection/hash.hpp
+++ include/reflection/hash.hpp
@@ -1,7 +1,6 @@
 #pragma once
-#include <regex>
-#include <string>
 #include <string_view>
+#include "reflection/name.hpp"
 #include "structures/tstring.hpp"
 
 namespace atom::utils {
@@ -21,64 +20,20 @@ constexpr inline std::size_t hash(std::string_view string) {
     return value;
 }
 
-inline std::string process_name(const std::string& str) {
-    std::string result = str;
-    std::regex prefix_pattern(R"(class\ |struct\ |enum\ )");
-    result = std::regex_replace(result, prefix_pattern, "");
-    std::regex comma_pattern(R"(,\ |\ ,)");
-    result = std::regex_replace(result, comma_pattern, ",");
-    std::regex endtparam_pattern(R"(> |\ >)");
-    result = std::regex_replace(result, endtparam_pattern, ">");
-    std::regex pattern(R"(:\ |\ :)");
-    result = std::regex_replace(result, pattern, ":");
-    result = std::regex_replace(result, pattern, ":");
-
-    return result;
-}
-
-inline bool valid_name(const std::string& str) {
-    std::regex pattern(R"(class\ |struct\ |enum\ |,\ |\ ,|>\ |\ >|:\ |\ :)");
-    return !std::regex_match(str, pattern);
-}
-
 } // namespace internal
 /*! @endcond */
 
-/**
- * @brief Get the hash value of a string view.
- *
- * @return constexpr std::size_t Hash value.
- */
-constexpr std::size_t hash(std::string_view str) {
-    if (const auto found = str.find_last_of(' ');
-        found != std::string_view::npos && found < str.length()) {
-        return internal::hash(str.substr(found + 1));
-    }
-    else {
-        return internal::hash(str);
-    }
-}
-
-/**
- * @brief Get the hash value of a template string.
- *
- * @tparam Str Template string.
- * @return consteval std::size_t Hash value.
- */
 template <tstring_v Str>
-consteval std::size_t hash() {
-    return hash(Str.val);
+constexpr inline size_t hash_of() {
+    return internal::hash(Str.val);
 }
 
-/**
- * @brief Get the hash value of a type.
- *
- * @tparam Ty The type.
- */
 template <typename Ty>
-requires(!is_tstringv_v<Ty>)
-std::size_t hash() {
-    return hash(internal::process_name(typeid(Ty).name()));
+constexpr inline size_t hash_of() {
+    constexpr auto name = name_of<Ty>();
+    return internal::hash(name);
 }
+
+inline size_t hash_of(std::string_view str) { return internal::hash(str); }
 
 } // namespace atom::utils
