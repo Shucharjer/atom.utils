e7b5e7d31772fc7b79f2e1d467ff80decee86bdd
Shucharjer <Shucharjer@outlook.com>
2025-02-21 17:23:55 +0800

    add macros about inline

--- include/core/langdef.hpp
+++ include/core/langdef.hpp
@@ -7,3 +7,14 @@
 #else
     #error "Macro 'CPP23' has been defined for unknown use."
 #endif
+
+#ifdef _MSC_VER
+    #define ATOM_FORCE_INLINE __forceinline
+    #define ATOM_FORCE_NOINLINE __declspec(noinline)
+#elif defined(__GNUC__) || defined(__clang__)
+    #define ATOM_FORCE_INLINE inline __attribute__((__always_inline__))
+    #define ATOM_NOINLINE __attribute__((__noinline__))
+#else
+    #define ATOM_FORCE_INLINE inline
+    #define ATOM_NOINLINE
+#endif
