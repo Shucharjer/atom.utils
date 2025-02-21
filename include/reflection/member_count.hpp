2e7ccdc6bf913664318da3d3fb32d30aa3f8b9a1
Shucharjer <Shucharjer@outlook.com>
2025-02-21 17:23:17 +0800

    removed files in reflection folder

--- include/reflection/member_count.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "reflection/aggregate/member_count.hpp"
-#include "reflection/others/member_count.hpp"
-
-namespace atom::utils {
-template <typename Ty>
-requires std::is_aggregate_v<Ty> || concepts::has_field_traits<Ty>
-constexpr inline size_t member_count_v = member_count_of<Ty>();
-}
