867beba92e037139e3629605e1b8ccc4d7492a46
Shucharjer <Shucharjer@outlook.com>
2025-02-21 17:27:32 +0800

    test case fit for current reflection framework

--- test/reflection.cpp
+++ test/reflection.cpp
@@ -1,62 +1,81 @@
-#ifdef TEST_UTILS
-    #include "reflection.hpp"
-    #include <catch.hpp>
-    #include "output.hpp"
-    #include "reflection/macros.hpp"
-    #include "reflection/registery.hpp"
+#include "reflection.hpp"
+#include "require.hpp"
 
 using namespace atom;
 
-struct reflected_class {
+struct aggregate {
     int member1;
     char member2;
 };
 
-BEGIN_TYPE(reflected_class)
-FIELDS(FIELD(member1), FIELD(member2))
-END_TYPE()
-REGISTER(reflected_class, reflected_class_register)
+class not_aggregate {
+public:
+    constexpr not_aggregate(int v1, char v2) noexcept : another_member1(v1), another_member2(v2) {}
+    constexpr not_aggregate(const not_aggregate&)            = default;
+    constexpr not_aggregate(not_aggregate&&)                 = default;
+    constexpr not_aggregate& operator=(const not_aggregate&) = default;
+    constexpr not_aggregate& operator=(not_aggregate&&)      = default;
+    constexpr ~not_aggregate()                               = default;
 
-TEST_CASE("reflection") {
-    SECTION("reflected") {
-        auto inst      = reflected_class{};
-        auto reflected = utils::reflected<reflected_class>{};
-        REQUIRE(reflected.name() == "reflected_class");
+    REFL_MEMBERS(not_aggregate, another_member1, another_member2)
+private:
+    int another_member1;
+    char another_member2;
+};
 
-        auto& fields  = reflected.fields();
-        auto& traits1 = std::get<0>(fields);
-        REQUIRE(traits1.name() == "member1");
-
-        const auto test_num = 114514;
-        inst.member1        = test_num;
-        REQUIRE(traits1.get(inst) == test_num);
-
-        constexpr auto index = utils::index_of<"member1">(fields);
-        REQUIRE(index == 0);
+int main() {
+    // member_count_of
+    {
+        REQUIRES(utils::member_count_of<aggregate>() == 2);
+        REQUIRES(utils::member_count_of<not_aggregate>() == 2);
     }
 
-    SECTION("registry") {
-        SECTION("all") {
-            auto&& reflecteds =
-                utils::registry<utils::basic_constexpr_extend, utils::constexpr_extend>::all();
-            auto takes = reflecteds | std::views::filter([](const auto& reflected) {
-                             return reflected->name() == "reflected_class";
-                         });
-            REQUIRE_FALSE(takes.empty());
-        }
+    // get
+    {
+        const auto num = 114514;
+        const char ch  = '!';
+        auto a         = aggregate{ .member1 = num, .member2 = ch };
+        auto na        = not_aggregate{ num, ch };
 
-        SECTION("identity") {
-            const auto hash         = utils::hash<reflected_class>();
-            const auto de_reflected = utils::reflected<reflected_class>();
-            print(de_reflected.name());
-            newline();
-            REQUIRE(de_reflected.hash() == hash);
+        REQUIRES(utils::get<0>(a) == num);
+        REQUIRES(utils::get<1>(a) == ch);
 
-            const auto identity   = utils::basic_registry::identity(hash);
-            const auto& reflected = utils::basic_registry::find<reflected_class>();
-            REQUIRE(hash == reflected->hash());
-        }
+        REQUIRES(utils::get<0>(na) == num);
+        REQUIRES(utils::get<1>(na) == ch);
+    }
+
+    // member_names_of
+    {
+        auto names_a  = utils::member_names_of<aggregate>();
+        auto names_na = utils::member_names_of<not_aggregate>();
+
+        REQUIRES(names_a[0] == "member1");
+        REQUIRES(names_a[1] == "member2");
+
+        REQUIRES(names_na[0] == "another_member1");
+        REQUIRES(names_na[1] == "another_member2");
+    }
+
+    // description_of
+    {
+        auto description_a  = utils::description_of<aggregate>();
+        auto description_na = utils::description_of<not_aggregate>();
+
+        REQUIRES(utils::authenticity_of(description_a, utils::bits::is_aggregate));
+        REQUIRES(!utils::authenticity_of(description_na, utils::bits::is_aggregate));
+    }
+
+    // reflected
+    {
+        auto reflected_a  = utils::reflected<aggregate>();
+        auto reflected_na = utils::reflected<not_aggregate>();
+
+        using namespace utils::bits;
+
+        auto description_a  = reflected_a.description();
+        auto description_na = reflected_na.description();
+
+        REQUIRES(utils::authenticity_of(description_a, is_aggregate))
+        REQUIRES_FALSE(utils::authenticity_of(description_na, is_aggregate))
     }
 }
-
-#endif
