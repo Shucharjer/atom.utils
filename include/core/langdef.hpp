22d31f785d2f3a933372b74b854000bc1c2af8ef
Shucharjer <Shucharjer@outlook.com>
2025-02-24 00:41:29 +0800

    added attribute for allocator

--- include/core/langdef.hpp
+++ include/core/langdef.hpp
@@ -11,10 +11,13 @@
 #ifdef _MSC_VER
     #define ATOM_FORCE_INLINE __forceinline
     #define ATOM_FORCE_NOINLINE __declspec(noinline)
+    #define ALLOCATOR __declspec(allocator)
 #elif defined(__GNUC__) || defined(__clang__)
     #define ATOM_FORCE_INLINE inline __attribute__((__always_inline__))
     #define ATOM_NOINLINE __attribute__((__noinline__))
+    #define ALLOCATOR __attribute__((malloc))
 #else
     #define ATOM_FORCE_INLINE inline
     #define ATOM_NOINLINE
+    #define ALLOCATOR
 #endif
