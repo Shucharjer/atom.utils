4909ee1b8271524659e646282b3be3d0b087ec68
Shucharjer <Shucharjer@outlook.com>
2025-02-25 18:43:56 +0800

    update ranges.hpp: forward declration of ranges::to

--- include/ranges.hpp
+++ include/ranges.hpp
@@ -10,4 +10,14 @@ template <std::ranges::input_range Rng>
 class phony_input_iterator;
 }
 
+template <typename Container, std::ranges::input_range Rng, typename... Args>
+requires(!std::ranges::view<Container>)
+[[nodiscard]] constexpr Container to(Rng&& range, Args&&... args);
+
+template <typename Container, typename... Args>
+[[nodiscard]] constexpr auto to(Args&&... args);
+
+template <template <typename...> typename Container, typename... Args>
+[[nodiscard]] constexpr auto to(Args&&... args);
+
 } // namespace atom::utils
