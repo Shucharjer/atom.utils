a1ab82e08735edebe676c3c14d36343d1d78e8ba
Shucharjer <Shucharjer@outlook.com>
2025-03-24 15:25:28 +0800

    add println to output.hpp

--- test/output.hpp
+++ test/output.hpp
@@ -3,4 +3,6 @@
 
 const inline auto print = [](const auto& val) { std::cout << val << ' '; };
 
+const inline auto println = [](const auto& val) { std::cout << val << '\n'; };
+
 const inline auto newline = []() { std::cout << '\n'; };
