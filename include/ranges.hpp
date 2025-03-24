93d5757ce826b5cb91bc4a2ccc5b5d76f9849f03
Shucharjer <Shucharjer@outlook.com>
2025-03-24 16:46:55 +0800

    move to() into ranges; forward declare element_iterator, elements_view and element_fn; add namespace views

--- include/ranges.hpp
+++ include/ranges.hpp
@@ -8,7 +8,6 @@ namespace atom::utils {
 namespace ranges {
 template <std::ranges::input_range Rng>
 class phony_input_iterator;
-}
 
 template <typename Container, std::ranges::input_range Rng, typename... Args>
 requires(!std::ranges::view<Container>)
@@ -19,5 +18,20 @@ template <typename Container, typename... Args>
 
 template <template <typename...> typename Container, typename... Args>
 [[nodiscard]] constexpr auto to(Args&&... args);
+
+template <std::ranges::range Rng, size_t Index, bool IsConst>
+struct element_iterator;
+
+template <std::ranges::input_range Vw, size_t Index>
+class elements_view;
+
+template <size_t Index>
+struct element_fn;
+
+namespace views {}
+
+} // namespace ranges
+
+namespace views = ranges::views;
 
 } // namespace atom::utils
