f11318ba4a6c96bb10604bedd4af04ddaead998a
Shucharjer <Shucharjer@outlook.com>
2025-06-08 13:20:48 +0800

    removed basic_tstring & tstring

--- include/structures/tstring.hpp
+++ include/structures/tstring.hpp
@@ -2,42 +2,8 @@
 #include <cstddef>
 #include <cstring>
 #include <ostream>
-#include <string_view>
 
 namespace atom::utils {
-
-class basic_tstring {
-public:
-    constexpr basic_tstring(const char* string) : view(string) {}
-    constexpr basic_tstring(const basic_tstring& obj) = default;
-    constexpr basic_tstring(basic_tstring&& obj) noexcept : view(obj.view) {}
-    constexpr basic_tstring& operator=(const basic_tstring&) = delete;
-    constexpr basic_tstring& operator=(basic_tstring&&)      = delete;
-    constexpr virtual ~basic_tstring()                       = default;
-
-    [[nodiscard]] constexpr auto get() -> std::string_view& { return view; }
-    [[nodiscard]] constexpr auto get() const -> const std::string_view& { return view; }
-
-    [[nodiscard]] auto operator<=>(const basic_tstring& obj) const {
-        return std::strcmp(view.data(), obj.view.data());
-    }
-
-    [[nodiscard]] auto operator==(const basic_tstring& obj) const -> bool {
-        return view == obj.view;
-    }
-
-    [[nodiscard]] auto operator!=(const basic_tstring& obj) const -> bool {
-        return view != obj.view;
-    }
-
-    std::ostream& operator<<(std::ostream& stream) {
-        stream << view;
-        return stream;
-    }
-
-private:
-    std::string_view view;
-};
 
 /**
  * @brief String that could be used as template argument
@@ -49,9 +15,9 @@ private:
  * Declare a function template like `template <tstring String>
  * void foo();`, then use it like `foo<"example">();`
  */
-template <std::size_t N>
+template <std::size_t N, typename Char = char>
 struct tstring_v {
-    constexpr tstring_v(const char (&arr)[N]) {
+    constexpr tstring_v(const Char (&arr)[N]) {
         // memcpy is not a compile-time function
         // std::memcpy(val, arr, N);
         std::copy(arr, arr + N, val);
@@ -77,7 +43,7 @@ struct tstring_v {
         return stream;
     }
 
-    char val[N]{};
+    Char val[N];
 };
 
 template <typename>
@@ -88,36 +54,5 @@ struct is_tstringv<tstring_v<N>> : std::true_type {};
 
 template <typename Ty>
 constexpr bool is_tstringv_v = is_tstringv<Ty>::value;
-
-/**
- * @brief This type provides a way to obtain compile-time template string constants.
- *
- * @tparam String
- */
-template <tstring_v String>
-class tstring : public basic_tstring {
-public:
-    constexpr tstring() : basic_tstring(String.val) {}
-
-    template <tstring_v Str>
-    [[nodiscard]] constexpr auto operator<=>(const tstring<Str>& obj) const {
-        return String <=> Str;
-    }
-
-    template <tstring_v Str>
-    [[nodiscard]] constexpr auto operator==(const tstring<Str>& obj) const -> bool {
-        return String == Str;
-    }
-
-    template <tstring_v Str>
-    [[nodiscard]] constexpr auto operator!=(const tstring<Str>& obj) const -> bool {
-        return String != Str;
-    }
-
-    std::ostream& operator<<(std::ostream& stream) const {
-        stream << get();
-        return stream;
-    }
-};
 
 } // namespace atom::utils
